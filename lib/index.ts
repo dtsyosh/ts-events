7dfcdb34d9cee8719aee7c57d62f3440a11b2751
Diego Tsuyoshi <diego@pointr.co>
2022-04-30 23:41:23 -0400

    fix: exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
 export { TriggersOn } from './decorators/triggers-on'
+export { TriggersOnClass } from './decorators/triggers-on-class'
 export { PointrEvents } from './factory'
 export { PointrEventListener } from './interfaces'
